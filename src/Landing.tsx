f2432771aff257d3e7f4c5d62adffb42598933eb
Orinah20 <orinah.nyandieka@gmail.com>
2022-08-01 17:39:30 +0300

    Creation of the Login page.

---
+++ src/Landing.tsx
@@ -0,0 +1,8 @@
+function LandingPage(){
+    return (
+        <div>Hi There</div>
+    )
+}
+
+export default LandingPage;
+
---
+++ src/Login.tsx
@@ -0,0 +1,96 @@
+import vector_left from './svg/vector_left.svg';
+import vector_right from './svg/vector_right.svg';
+import agent from './svg/agent.svg'
+import sky_logo_login from "./svg/sky_logo_login.svg"
+import './App.css';
+import {useForm} from "@mantine/form";
+import {Group, PasswordInput, TextInput, Button} from "@mantine/core";
+import LandingPage from "./Landing";
+
+function Login() {
+    //
+    const form = useForm({
+        initialValues: {
+            email: '',
+            password: '',
+
+            //dummyvalidation
+
+            confirmEmail: 'forinan2000@gmail.com',
+            confirmPassword: 'Qwerty123$'
+        },
+
+        validate: {
+
+            //dummyvalidation
+
+            email: (value, values) => (/^\S+@\S+$/.test(value) ?
+                (value !== values.confirmEmail ? 'Wrong Email' : null) : 'Invalid email'),
+            password: (value, values) =>
+                value !== values.confirmPassword ? 'Wrong Password' : null,
+        },
+    });
+
+    return (
+        <div>
+            <div>
+                <div className={"vector"}>
+                    <img className={"vector-left"} alt={""} src={vector_left}/>
+                    <img className={"vector-right"} alt={""} src={vector_right}/>
+                </div>
+
+                <div>
+                    <div className={"central_container"}>
+                        <img className={"agent_vector"} alt={""} src={agent}/>
+                        <img className={"login_logo"} alt={""} src={sky_logo_login}/>
+                        <span className={"login_area"}>
+                            <div className={"form_header"}>Login</div>
+                            <div className={"form_content"}>
+                                <form onSubmit={form.onSubmit((values) => <LandingPage/>)}>
+                                <TextInput
+                                    required
+                                    style={{cursor:"pointer"}}
+                                    mt="md"
+                                    label="Email Address"
+                                    placeholder="Email Address"
+                                    {...form.getInputProps('email')}/>
+
+                                <PasswordInput
+                                    required
+                                    mt="md"
+                                    label="Password"
+                                    placeholder="Password"
+                                    {...form.getInputProps('password')} />
+
+
+                                <Group
+                                    position="right"
+                                    mt="md"
+                                >
+                                        <Button
+                                            size="xs"
+                                            type="submit"
+                                        >
+                                            Submit
+                                        </Button>
+                                </Group>
+
+                                <Group
+                                    position="right"
+                                >
+                                <a href={"/"} style={{fontSize: 14}}>
+                                    Forgot password?
+                                </a>
+                                </Group>
+
+                            </form>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
